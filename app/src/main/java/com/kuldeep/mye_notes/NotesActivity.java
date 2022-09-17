3ecded9be9f07bb6d291c292f7f6511c40012736
kuldeepbairwa <kuldeepbairwa8@gmail.com>
2022-09-17 11:32:46 +0530

    2nd_commit

--- app/src/main/java/com/kuldeep/mye_notes/NotesActivity.java
+++ app/src/main/java/com/kuldeep/mye_notes/NotesActivity.java
@@ -227,7 +227,7 @@ public class NotesActivity extends AppCompatActivity {
                 {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        finish();
+                        moveTaskToBack(true);
                     }
 
                 })
